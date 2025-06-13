58411a954f3c09092d0ddbc737c63f2fef965a7a
coluccifrancesco <coluccif2002@gmail.com>
2025-06-13 17:09:39 +0200

    Fix filter function to correctly return even numbers from the array

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,13 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+let evenNums = nums.filter((num) => {
+    if(num % 2 === 0){
+        return num
+    }
+    return 
+})
+
+console.log(evenNums);
+
 // Risultato: [2, 8, 4, 12]
