810d6d61f0783f2b3faf84e64412a308916ff991
coluccifrancesco <coluccif2002@gmail.com>
2025-06-13 17:17:04 +0200

    Add console logging for student retrieval by ID

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,9 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+console.log(students[1]);
+
+let id2 = students.filter(student => student.id === 2)
+console.log(id2);
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
