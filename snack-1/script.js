56d7a2c0d9ef1dea2bc8212b61dd25fddb18314d
coluccifrancesco <coluccif2002@gmail.com>
2025-06-16 11:26:43 +0200

    Add logging for names and zucchine filtering results

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,4 +3,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 console.log(names);
 
+names.forEach((nomi, i, names) => {
+    console.log(nomi);
+})
+
+for (let i = 0; i < names.length; i++) {
+    const nomi = names[i];
+    console.log(nomi);
+}
+
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
